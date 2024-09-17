9f9b495fdf9cef8eeebe2eb3e38e23a3253cf677
Aditya <adityasahoo652@gmail.com>
2024-09-17 18:03:53 +0530

    feat: Add react-hot-toast for displaying notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
         {children}
+        <Toaster />
       </body>
     </html>
   );
