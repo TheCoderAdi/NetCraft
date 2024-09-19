e21a89f46b87cea71ffda2c08588df4525a75fc2
Aditya <adityasahoo652@gmail.com>
2024-09-19 10:07:30 +0530

    chore: Update home page layout responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,19 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-screen  p-8 text-black font-sans">
-        <h1 className="text-5xl font-extrabold text-gray-900 tracking-tight leading-tight mb-6">
+      <div className="flex flex-col items-center justify-center min-h-screen p-8 text-black font-sans ">
+        <h1 className="text-5xl font-extrabold text-gray-900 tracking-tight leading-tight mb-6 max-sm:text-center max-sm:text-3xl">
           Discover <span className="text-purple-500">Network Topologies</span>
         </h1>
 
-        <p className="text-lg max-w-2xl text-center text-gray-700 mb-8">
+        <p className="text-lg max-w-2xl text-center text-gray-700 mb-8 max-sm:text-base">
           Explore and visualize the structure of different network topologies
           such as Mesh, Star, Ring, and Bus. Understand their unique
           characteristics and how they operate.
         </p>
 
         <div className="bg-white p-6 shadow-lg rounded-lg w-full max-w-xl">
-          <h2 className="text-2xl font-bold text-gray-800 mb-4">
+          <h2 className="text-2xl font-bold text-gray-800 mb-4 max-sm:text-lg">
             Available Topologies
           </h2>
           <ul className="list-disc pl-5 space-y-2 text-gray-700">
@@ -42,7 +42,7 @@ export default function Home() {
           <button
             className={`${buttonVariants({
               variant: "default",
-            })} mt-8 w-40 h-14 rounded-full`}
+            })} mt-8 w-40 h-14 rounded-full p-20 `}
           >
             Visualize Now
           </button>
